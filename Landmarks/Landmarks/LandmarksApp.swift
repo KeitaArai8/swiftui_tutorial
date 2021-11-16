dca2d9e0bf2ec4456c7777d5d63e25992f9ca047
Keita Arai <ynwano.88@gmail.com>
2021-11-16 23:08:23 +0900

    first

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Keita Arai on 2021/11/16.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
